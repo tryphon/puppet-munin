ce1af4f481dd5c907a3eafc0c555e1e3907dc458
Alban Peignier <alban.peignier@free.fr>
2009-12-19 16:07:25 +0100

    Create munin::anode::default_plugins class with default settings

--- manifests/init.pp
+++ manifests/init.pp
@@ -47,6 +47,33 @@ class munin {
       require => Package[munin-node]
     }
 
+    # Directory to store local plugins
+    file { ["/usr/local/share/munin", "/usr/local/share/munin/plugins"]:
+      ensure => directory
+    }
+
+    class default_plugins {
+      
+      # some default plugins
+      munin::plugin { [ cpu, df, iostat, load, memory, netstat, swap ]: }
+      munin::plugin { if_eth0: script => if_, require => Package[ethtool] }
+
+      package { ethtool: }
+
+      # remove some debian default plugin
+      munin::plugin {
+        [ open_inodes, open_files, irqstats, interrupts, entropy, df_inode, if_err_eth0, if_err_eth1, vmstat, forks ]:
+        ensure => absent
+      }
+
+      # disable the (verbose) cron for /etc/munin/plugins/apt_all
+      exec { "sed -i '/apt_all/ s/^\\(.*\\)$/# \\1/' /etc/cron.d/munin-node":
+        unless => "/bin/grep -q '#.*apt_all' /etc/cron.d/munin-node",
+        require => Package["munin-node"]
+      }
+      
+    }
+
   }
 
 
