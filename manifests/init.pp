659335d185319039999faeda0152128ebfef4d22
Alban Peignier <alban@tryphon.eu>
2015-02-20 09:02:36 +0100

    Remove explicity common import. Remove some double quotes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,3 @@
-import "common" # require concatenated_file defines
-
 class munin {
 
   class server {
@@ -8,34 +6,34 @@ class munin {
     if $debian::lenny {
       include apt::backports
       apt::preferences { munin:
-        package => munin, 
-        pin => "release a=lenny-backports",
+        package => munin,
+        pin => 'release a=lenny-backports',
         priority => 999,
         before => Package[munin]
       }
     }
 
-    concatenated_file { "/etc/munin/munin.conf":
-      dir => "/etc/munin/conf.d",
+    concatenated_file { '/etc/munin/munin.conf':
+      dir => '/etc/munin/conf.d',
       require => Package[munin]
     }
 
-    concatenated_file_source { "00munin.conf.header":
-      dir    => "/etc/munin/conf.d",
-      source => "puppet:///munin/munin.conf.header"
+    concatenated_file_source { '00munin.conf.header':
+      dir    => '/etc/munin/conf.d',
+      source => 'puppet:///munin/munin.conf.header'
     }
 
-    concatenated_file_source { "munin.conf.local":
-      dir    => "/etc/munin/conf.d",
-      source => "puppet:///files/munin/munin.conf"
+    concatenated_file_source { 'munin.conf.local':
+      dir    => '/etc/munin/conf.d',
+      source => 'puppet:///files/munin/munin.conf'
     }
 
     # munin user can't create the log files and
     # munin tools can fail when log file is missing
-    file { [ "/var/log/munin/munin-update.log", 
-      "/var/log/munin/munin-graph.log", 
-      "/var/log/munin/munin-html.log", 
-      "/var/log/munin/munin-limits.log" ] :
+    file { [ '/var/log/munin/munin-update.log',
+      '/var/log/munin/munin-graph.log',
+      '/var/log/munin/munin-html.log',
+      '/var/log/munin/munin-limits.log' ] :
       ensure => present,
       owner => munin,
       group => adm,
@@ -43,7 +41,7 @@ class munin {
     }
 
     # TODO squeeze munin use /var/cache/munin/www
-    file { "/var/www/munin":
+    file { '/var/www/munin':
       ensure => directory,
       owner => munin,
       group => munin,
@@ -61,28 +59,28 @@ class munin {
     if $debian::lenny {
       include apt::backports
       apt::preferences { munin-node:
-        package => munin-node, 
-        pin => "release a=lenny-backports",
+        package => munin-node,
+        pin => 'release a=lenny-backports',
         priority => 999,
-        before => Package["munin-node"]
+        before => Package['munin-node']
       }
     }
 
     service { munin-node:
       ensure    => running,
-      subscribe => [Package[munin-node], File["/etc/munin/munin-node.conf"]]
+      subscribe => [Package[munin-node], File['/etc/munin/munin-node.conf']]
     }
-    file { "/etc/munin/munin-node.conf":
+    file { '/etc/munin/munin-node.conf':
       require => Package[munin-node]
     }
 
     # Directory to store local plugins
-    file { ["/usr/local/share/munin", "/usr/local/share/munin/plugins"]:
+    file { ['/usr/local/share/munin', '/usr/local/share/munin/plugins']:
       ensure => directory
     }
 
     class default_plugins {
-      
+
       # some default plugins
       munin::plugin { [ cpu, df, iostat, load, memory, netstat, swap ]: }
       munin::plugin { if_eth0: script => if_, require => Package[ethtool] }
@@ -98,16 +96,16 @@ class munin {
       # disable the (verbose) cron for /etc/munin/plugins/apt_all
       exec { "sed -i '/apt_all/ s/^\\(.*\\)$/# \\1/' /etc/cron.d/munin-node":
         unless => "/bin/grep -q '#.*apt_all' /etc/cron.d/munin-node",
-        require => Package["munin-node"]
+        require => Package['munin-node']
       }
-      
+
     }
 
     include munin::anode::tiger
 
   }
 
-  define plugin($ensure = "present", $script_path = "/usr/share/munin/plugins", $script = '', $config = '', $source = '') {
+  define plugin($ensure = 'present', $script_path = '/usr/share/munin/plugins', $script = '', $config = '', $source = '') {
     include munin::anode
 	  debug ( "munin_plugin: name=$name, ensure=$ensure, script_path=$script_path" )
 
@@ -127,35 +125,35 @@ class munin {
         before => File[$plugin]
       }
 
-      $real_script_path = "/usr/local/share/munin/plugins"
+      $real_script_path = '/usr/local/share/munin/plugins'
     } else {
       $real_script_path = $script_path
     }
 
 	  case $ensure {
-		  "absent": {
+		  'absent': {
 			  debug ( "munin_plugin: suppressing $plugin" )
 			  file { $plugin:
 			    ensure => absent,
-				  require => Package["munin-node"],
-				  notify => Service["munin-node"]
+				  require => Package['munin-node'],
+				  notify => Service['munin-node']
 			  }
 		  }
 		  default: {
         case $script {
           '': {
-            $plugin_src = $ensure ? { "present" => $name, default => $ensure }
+            $plugin_src = $ensure ? { 'present' => $name, default => $ensure }
           }
           default: {
             $plugin_src = $script
           }
         }
 		    debug ( "munin_plugin: making $plugin using src: $plugin_src" )
-        
+
 		    file { $plugin:
           ensure => "$real_script_path/${plugin_src}",
-	        require => Package["munin-node"],
-	        notify => Service["munin-node"],
+	        require => Package['munin-node'],
+	        notify => Service['munin-node'],
         }
       }
     }
@@ -188,7 +186,7 @@ class munin {
       }
     }
   }
-  
+
 }
 
 class munin::anode::tiger {
