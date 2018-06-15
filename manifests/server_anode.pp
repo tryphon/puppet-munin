185ac8d01aa349e725de28ee2a238423cce30f85
Florent Peyraud <florent.peyraud@gmail.com>
2018-06-15 15:56:09 +0200

    extra limits for server_anode

--- manifests/server_anode.pp
+++ manifests/server_anode.pp
@@ -1,4 +1,4 @@
-define munin::server_anode($ip_address = $name, $load_warning = 0.4, $load_critical = 0.7, $cpu_system_warning = 40, $cpu_system_critical = 80, $cpu_user_warning = 40, $cpu_user_critical = 80, $cpu_iowait_warning = 60, $cpu_iowait_critical = 80, $mem_app_warning = 0.7, $mem_app_critical = 0.95) {
+define munin::server_anode($ip_address = $name, $load_warning = 0.4, $load_critical = 0.7, $cpu_system_warning = 40, $cpu_system_critical = 80, $cpu_user_warning = 40, $cpu_user_critical = 80, $cpu_iowait_warning = 60, $cpu_iowait_critical = 80, $mem_app_warning = 0.7, $mem_app_critical = 0.95, $extra_limits = []) {
   file { "/etc/munin/munin-conf.d/node-${name}.conf":
     content => template('munin/node.conf'),
     require => Package['munin']
