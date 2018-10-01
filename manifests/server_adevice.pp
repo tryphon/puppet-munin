c3a01bc2c674c1efc250807d0be6a703551288bb
Florent Peyraud <florent.peyraud@gmail.com>
2018-10-01 18:18:15 +0200

    device_anode to server_adevice

--- manifests/server_adevice.pp
+++ manifests/server_adevice.pp
@@ -1,4 +1,4 @@
-define munin::device_anode($ip_address = 'localhost', $extra_limits = []) {
+define munin::server_adevice($ip_address = 'localhost', $extra_limits = []) {
   file { "/etc/munin/munin-conf.d/node-${name}.conf":
     content => template('munin/device.conf'),
     require => Package['munin']
